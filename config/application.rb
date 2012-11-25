0e258f632e003ce351bf088415cda59fa82da5b7
Percy Keeley <percy.keeley@softwareForest.com>
2012-11-25 08:25:16 -0500

    Update Assets 11/25/2012

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,8 @@ module Koyinsho
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false #PK added
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
