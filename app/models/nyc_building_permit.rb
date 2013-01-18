e9086482e58540d01b886590013fc64b84c18ed6
Percy Keeley <percy.keeley@softwareforest.com>
2013-01-18 12:35:13 -0500

    NYC Building Permit part-3

--- app/models/nyc_building_permit.rb
+++ app/models/nyc_building_permit.rb
@@ -10,7 +10,7 @@ class NycBuildingPermit < ActiveRecord::Base
   attr_accessible :permit_sequence, :city_state_zip, :job, :borough, :filing_date, :owner_s_house_street, :permit_status, :permittee_s_phone
   attr_accessible :lot, :permittee_s_business_name, :permittee_s_license, :bin, :job_doc, :owner_s_business_type, :site_safety_mgr_business_name
   attr_accessible :special_district_2, :residential, :self_cert, :permit_sequence, :oil_gas, :site_safety_mgr_s_name
-  attr_accessible :superintendent_first_last_name, :superintendent_business_name, :non_profit
+  attr_accessible :superintendent_first_last_name, :superintendent_business_name, :non_profit, :hic_license
 
   def self.nyc_opendata_load(nyc_opendata,record_limit=nil)
   	url = 'http://'
@@ -26,10 +26,40 @@ class NycBuildingPermit < ActiveRecord::Base
  	end
  	nyc_permit_count = 0
  	nyc_permits.each do |nyc_permit|
+ 	  [:owner_s_first_last_name,
+		:permittee_s_first_last_name,
+		:site_safety_mgr_s_name, 
+		:superintendent_first_last_name].each do |name|
+		  if nyc_permit[name]
+		     nyc_permit[name] = normalized_name(nyc_permit[name])
+		   end
+	  end
  	  nyc_permit_count += 1 if NycBuildingPermit.create(nyc_permit)	
  	end
   	nyc_permit_count
   end
  
+  private
+
+  def self.normalized_name(first_m_last_name)
+  	return first_m_last_name unless first_m_last_name.kind_of?(String)
+  	
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? ' -'
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? '- '
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? '/'
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? ' JR.'
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? ' SR.'
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? ','
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? '+'
+  	return "[#{first_m_last_name}]" if first_m_last_name.include? '&'
+
+
+  	split_names = first_m_last_name.split(' ').map {|w| w.capitalize}
+  	return "[#{first_m_last_name}]" if split_names.size <= 1
+  	return "[#{first_m_last_name}]" if split_names[-1].include? 'Jr'
+  	return "[#{first_m_last_name}]" if split_names[-1].include? 'Sr'
+  	last_first_m_name = "#{split_names[-1]}, #{split_names[0..(split_names.size - 2)].join(' ')}"
+  end
+
 end
  
--- spec/models/nyc_building_permit_spec.rb
+++ spec/models/nyc_building_permit_spec.rb
@@ -5,29 +5,29 @@ describe NycBuildingPermit do
   	NycBuildingPermit.nyc_opendata_load(ENV['NYC_OPEN_DATA_PERMITS'],10)
   end
 
-  it 'are updating NycBuildingPermits' do
+  it 'updates NycBuildingPermits' do
   	NycBuildingPermit.count.should > 0
   end
 
-  xit 'are updating PropertyOwners' do
+  it 'updates PropertyOwners' do
   	PropertyOwner.count.should_not be(0)
   end
 
-  xit 'are updating LicensedContractors' do
+  xit 'updates LicensedContractors' do
   	LicensedContractor.count.should_not be(0)
   end
 
-  xit 'are updating PropertyBuildings' do
+  xit 'updates PropertyBuildings' do
   	PropertyBuilding.count.should_not be(0)
   end
 
-  xit 'are updating Permits' do
+  xit 'updates Permits' do
   	Permit.count.should_not be(0)
   end
 
   after :all do
   	NycBuildingPermit.delete_all
-  	# PropertyOwner.delete_all
+  	PropertyOwner.delete_all
   	# LicensedContractor.delete_all
   	# PropertyBuilding.delete_all
   	# Permit.delete_all
